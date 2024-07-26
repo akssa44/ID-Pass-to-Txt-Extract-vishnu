063f96499239316b15124696ddee54f9289c6a65
akssa44 <vinaysharma0011223344@gmail.com>
2024-07-27 02:48:53 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,7 +3,7 @@ import os
 api_id = 27215374
 api_hash = os.environ.get("API_HASH", "b4495d7b9db5bb280121b4eeeb68331c")
 bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "6329158981")
+auth_users = os.environ.get("AUTH_USERS", "6762856879")
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "6329158981")
+osowner_users = os.environ.get("OWNER_USERS", "6762856879")
 owner_users = [int(num) for num in osowner_users.split(",")]
