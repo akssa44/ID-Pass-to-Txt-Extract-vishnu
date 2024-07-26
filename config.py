81e07072c32c98320db6f8dbc1dbccbe2ceda3cb
akssa44 <vinaysharma0011223344@gmail.com>
2024-07-27 02:51:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28528007
-api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6329158981"
+api_id = 27215374
+api_hash = "b4495d7b9db5bb280121b4eeeb68331c"
+bot_token = "7244371795:AAE0XPlpDzHNXfHMPZpbhDIbZ4W27pawhRw"
+auth_users = "6762856879"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6329158981"
+osowner_users = "6762856879"
 owner_users = [int(num) for num in osowner_users.split(",")]
