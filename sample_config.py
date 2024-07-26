96807ab439d652e5bf06a7df1c21446613aa561d
akssa44 <vinaysharma0011223344@gmail.com>
2024-07-27 02:24:05 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id = 28528007
-api_hash = os.environ.get("API_HASH", "38464da16c80310cabc8d13952419cf3")
+api_id = 27215374
+api_hash = os.environ.get("API_HASH", "b4495d7b9db5bb280121b4eeeb68331c")
 bot_token = os.environ.get("BOT_TOKEN")
 auth_users = os.environ.get("AUTH_USERS", "6329158981")
 sudo_users = [int(num) for num in auth_users.split(",")]
